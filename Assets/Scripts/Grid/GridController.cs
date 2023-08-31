f913b4b58884108281c267cbc13aa2ac3304bf59
Tomislav Jukica <tomislav.jukica07@gmail.com>
2023-08-31 13:26:06 +0200

    Fix Y rotation.

--- Assets/Scripts/Grid/GridController.cs
+++ Assets/Scripts/Grid/GridController.cs
@@ -11,48 +11,84 @@ public class GridController : MonoBehaviour
     public Vector3 StartPosition;
     public Vector3 FinishPosition;
 
-    private Vector3 _currentRotation;
-    private Vector3 _lastRotation;
-    private bool _isLastRotationSet = false;
+    private Vector3 _handRotation = Vector3.zero;
+    private Vector3 _lastRotation = Vector3.zero;
+    private bool _isHandStartSet = false;
     private bool _rotating = false;
+
+    private Vector3 _handStart = Vector3.zero;
 
     Vector3 offset;
     private void Update()
     {
-        if(_rotating && CanMove)
+        if (_rotating && CanMove)
         {
-            if (!_isLastRotationSet)
+            if (_isHandStartSet)
             {
-                offset = rightHand.transform.eulerAngles;
-                _isLastRotationSet = true;
+                offset = new(0, _handStart.y - rightHand.transform.eulerAngles.y, 0);
+
+                _handRotation = new Vector3(0, offset.y, 0); //150 | 0 | 10
+
+                if (_handRotation != Vector3.zero)
+                {
+                    print(_handRotation.y);
+                    transform.eulerAngles = new Vector3(0, _lastRotation.y + _handRotation.y, 0); //150 + 0 | skiped | 150 + 10         
+                }
+            }
+            else
+            {
+                _isHandStartSet = true;
+                _handStart = new(0, rightHand.transform.eulerAngles.y, 0);
             }
 
-            if (_currentRotation != rightHand.transform.eulerAngles)
-            {
-                transform.eulerAngles -= rightHand.transform.eulerAngles - offset;
-                _currentRotation = rightHand.transform.eulerAngles;
-            }
+            
 
-            offset = rightHand.transform.eulerAngles;
         }
+        else
+        {
+            _lastRotation = transform.eulerAngles;
+
+            _isHandStartSet = false;
+            _handStart = Vector3.zero;
+        }
+
+
+
+
+        //if (!_isLastRotationSet)
+        //{
+        //    offset = rightHand.transform.eulerAngles;
+        //    _isLastRotationSet = true;
+        //}
+
+        //if (_currentRotation != rightHand.transform.eulerAngles)
+        //{
+        //    transform.eulerAngles -= rightHand.transform.eulerAngles - offset;
+        //    _currentRotation = rightHand.transform.eulerAngles;
+        //}
+
+        //offset = rightHand.transform.eulerAngles;
+
+
     }
 
-    
+
+
     public void GripPressed()
     {
-        _rotating = true;        
+        _rotating = true;
     }
 
     public void GripRelesed()
     {
-        _isLastRotationSet = false;
+        _isHandStartSet = false;
         _rotating = false;
     }
 
     public void ResetRotation()
     {
         transform.eulerAngles = Vector3.zero;
-        _currentRotation = Vector3.zero;
+        _handRotation = Vector3.zero;
     }
 
     public void SetFinishPosition()
